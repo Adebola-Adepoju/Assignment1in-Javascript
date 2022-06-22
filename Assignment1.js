c05159980c8fe8626ec63832dcac8d7221a5c3f7
Adepoju Adebola <adebolaadepoju76@gmail.com>
2022-06-22 12:49:11 +0100

    first commit

---
+++ Assignment1.js
@@ -0,0 +1,5 @@
+let myname ="Adepoju Adebola"
+let height= "1.74m"
+let country ="Nigeria"
+
+console.log("My name is" + " " + myname + " and I am" + " " + height + " tall." + " " + "I am from " + country + "." )
